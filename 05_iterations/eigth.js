736f5cb588915aaf3e4dfb19370842d4b07c7906
APOORV GUSAIN <32385555+apoorvgusain@users.noreply.github.com>
2024-02-02 16:40:18 +0000

    learn filter and map

---
+++ 05_iterations/eigth.js
@@ -0,0 +1,40 @@
+const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array1.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue,//uses initial value in first iteration accumulator 
+);
+
+console.log(sumWithInitial);
+// Expected output: 10
+
+
+const myNums = [1, 2, 3]
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 5)
+
+console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
